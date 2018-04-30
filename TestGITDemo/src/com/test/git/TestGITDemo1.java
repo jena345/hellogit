8aef28ab95362c5354cab53690b68e43e4aa3e32
aurus_training <aurus_training@aurus-training.aurustech.com>
2018-04-30 18:14:24 +0530

    # WARNING: head commit changed in the meantime
    m,ethod1

--- TestGITDemo/src/com/test/git/TestGITDemo1.java
+++ TestGITDemo/src/com/test/git/TestGITDemo1.java
@@ -7,6 +7,7 @@ public static void main(String[] args) {
 
 private static void m1(){
 	System.out.println("****************");
+	System.out.println("***************111*");
 }
 
 private static void m2(){
