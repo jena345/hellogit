c40e86b13f0f417e79b24e0c808f66dcb9b68da0
sjena <sjena@sjena-pc.aurustech.com>
2018-04-30 16:56:56 +0530

    aaaaaa

--- HelloSwitch/src/com/sjena/helloswitch/HelloSwitch1.java
+++ HelloSwitch/src/com/sjena/helloswitch/HelloSwitch1.java
@@ -11,5 +11,6 @@ public static void main(String[] args) {
 	System.out.println("log4j master-------- log b1---------");
 	System.out.println("log4j master-----------------");
 	System.out.println("log4j master----------log b33-------");
+	System.out.println("log4j master----------log b44-------");
 }
 }
