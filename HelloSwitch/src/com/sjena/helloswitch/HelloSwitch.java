db362c1ae6d48659a329252740650bf9521a276a
sjena <sjena@sjena-pc.aurustech.com>
2018-04-30 10:34:25 +0530

    test method commmted

--- HelloSwitch/src/com/sjena/helloswitch/HelloSwitch.java
+++ HelloSwitch/src/com/sjena/helloswitch/HelloSwitch.java
@@ -1,8 +1,12 @@
 package com.sjena.helloswitch;
 
 public class HelloSwitch {
-public static void main(String[] args) {
-	System.out.println("*****************************");
-	System.out.println("::::;;;;;;;;;;;;;;;;;;;;;;");
-}
+	public static void main(String[] args) {
+		System.out.println("*****************************");
+		System.out.println("::::;;;;;;;;;;;;;;;;;;;;;;");
+	}
+
+	private static void test() {
+
+	}
 }
